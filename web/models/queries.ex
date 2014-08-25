64721716f5d93e988a40367d32edfb3ded41d729
Kosmas Chatzimichalis <kosmas@mach7x.com>
2014-08-25 16:43:13 +0200

    change order of list so most recent is at the top

--- web/models/queries.ex
+++ web/models/queries.ex
@@ -3,6 +3,7 @@ defmodule JobBoard.Queries do
 
   def jobs_query do
     query = from job in JobBoard.Jobs,
+            order_by: [desc: job.id],
             select: job
     JobBoard.Repo.all(query)
   end
