6624ae0a4de160ffc4fa1c114459fe1c11ddf20b
Kosmas Chatzimichalis <kosmas@mach7x.com>
2014-08-25 16:37:18 +0200

    add new route, template and controller method

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -1,8 +1,19 @@
 defmodule JobBoard.PageController do
   use Phoenix.Controller
+  alias JobBoard.Router
 
   def index(conn, _params) do
     jobs = JobBoard.Queries.jobs_query
     render conn, "index", jobs: jobs
+  end
+
+  def new(conn, _params) do
+    render conn, "new"
+  end
+
+  def save(conn, params) do
+    job = %JobBoard.Jobs{title: params["title"], description: params["description"], job_type: params["type"], job_status: params["status"]}
+    JobBoard.Repo.insert(job)
+    redirect conn, Router.index_path(:index)
   end
 end
--- web/router.ex
+++ web/router.ex
@@ -2,5 +2,7 @@ defmodule JobBoard.Router do
   use Phoenix.Router
 
   plug Plug.Static, at: "/static", from: :job_board
-  get "/", JobBoard.PageController, :index, as: :page
+  get "/", JobBoard.PageController, :index, as: :index
+  get "/new", JobBoard.PageController, :new, as: :new
+  post "/new", JobBoard.PageController, :save, as: :save
 end
