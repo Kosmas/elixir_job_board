94e7251e971886013e73f65a8c8d246fd210c897
Kosmas Chatzimichalis <kosmas@mach7x.com>
2014-08-26 18:32:41 +0200

    added route, method and template for editing a job

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -21,4 +21,18 @@ defmodule JobBoard.PageController do
     job = JobBoard.Queries.job_detail_query(params["id"])
     render conn, "job", [job: job, action: params["action"]]
   end
+
+  def edit(conn, %{"id" => id}) do
+    job = JobBoard.Queries.job_detail_query(id)
+    render conn, "edit", job:  job
+  end
+
+  def update(conn, params) do
+    IO.inspect params["type"]
+    job = JobBoard.Queries.job_detail_query(params["id"])
+    job = %{job | title: params["title"], description: params["description"],
+                  job_type: params["type"], job_status: params["status"]}
+    JobBoard.Repo.update(job)
+    redirect conn, Router.index_path(:index)
+  end
 end
--- web/router.ex
+++ web/router.ex
@@ -6,4 +6,6 @@ defmodule JobBoard.Router do
   get "/new", JobBoard.PageController, :new, as: :new
   post "/new", JobBoard.PageController, :save, as: :save
   get "/job/:id", JobBoard.PageController, :job, as: :job
+  get "/job/:id/edit", JobBoard.PageController, :edit, as: :edit
+  post "/job/:id", JobBoard.PageController, :update, as: :update
 end
