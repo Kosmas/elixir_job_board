cdfa9402ac34256c55d8b944a41d320e9d17c9e3
Kosmas Chatzimichalis <kosmas@mach7x.com>
2014-09-10 17:25:43 +0200

    upgrade to elxir 1.0.0-rc1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule JobBoard.Mixfile do
   def project do
     [ app: :job_board,
       version: "0.0.1",
-      elixir: "~> 0.15.0",
+      elixir: "~> 1.0.0-rc1",
       elixirc_paths: ["lib", "web"],
       deps: deps ]
   end
