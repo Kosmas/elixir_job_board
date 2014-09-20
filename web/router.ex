1a6cb7403743e27a2db982ceb0119d6028af7883
Kosmas Chatzimichalis <kosmas@mach7x.com>
2014-09-20 17:19:42 +0200

    removed old routes and fixed controller names

--- web/router.ex
+++ web/router.ex
@@ -1,18 +1,8 @@
 defmodule JobBoard.Router do
   use Phoenix.Router
 
-#  plug Plug.Static, at: "/static", from: :job_board
-#  get "/", JobBoard.PageController, :index, as: :pages
-#  get "/new", JobBoard.PageController, :new, as: :new
-#  post "/new", JobBoard.PageController, :save, as: :save
-#  get "/job/:id", JobBoard.PageController, :job, as: :job
-#  get "/job/:id/edit", JobBoard.PageController, :edit, as: :edit
-#  post "/job/:id", JobBoard.PageController, :update, as: :update
-#  get "/job/:id/:action", JobBoard.PageController, :job, as: :delete
-#  post "/", JobBoard.PageController, :destroy, as: :destroy
-
   scope alias: JobBoard do
-    get "/", PagesController, :index, as: :index
-    resources "jobs", JobBoardController
+    get "/", PageController, :index, as: :pages
+    resources "jobs", JobController
   end
 end
