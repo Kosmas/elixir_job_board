e60c32d9bbda6537fb5733fca50d91fe3bf8d4aa
Kosmas Chatzimichalis <kosmas@mach7x.com>
2014-09-20 17:35:35 +0200

    removed methods for new, job, edit, update and delete from the pages controller as they are now in the job controller

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -6,39 +6,4 @@ defmodule JobBoard.PageController do
     jobs = JobBoard.Queries.jobs_query
     render conn, "index", jobs: jobs
   end
-
-  def new(conn, _params) do
-    render conn, "new"
-  end
-
-  def save(conn, params) do
-    job = %JobBoard.Jobs{title: params["title"], description: params["description"], job_type: params["type"], job_status: params["status"]}
-    JobBoard.Repo.insert(job)
-    redirect conn, Router.pages_path(:index)
-  end
-
-  def job(conn, params) do
-    job = JobBoard.Queries.job_detail_query(params["id"])
-    render conn, "job", [job: job, action: params["action"]]
-  end
-
-  def edit(conn, %{"id" => id}) do
-    job = JobBoard.Queries.job_detail_query(id)
-    render conn, "edit", job:  job
-  end
-
-  def update(conn, params) do
-    IO.inspect params["type"]
-    job = JobBoard.Queries.job_detail_query(params["id"])
-    job = %{job | title: params["title"], description: params["description"],
-                  job_type: params["type"], job_status: params["status"]}
-    JobBoard.Repo.update(job)
-    redirect conn, Router.pages_path(:index)
-  end
-
-  def destroy(conn, params) do
-    job = JobBoard.Queries.job_detail_query(params["id"])
-    JobBoard.Repo.delete(job)
-    redirect conn, Router.pages_path(:index)
-  end
 end
