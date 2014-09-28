cb4687bfd804633d4aab6dde93f24735c301bd28
Kosmas Chatzimichalis <kosmas@mach7x.com>
2014-09-28 23:27:00 +0200

    clean out code by using aliases2

--- web/controllers/job_controller.ex
+++ web/controllers/job_controller.ex
@@ -1,9 +1,12 @@
 defmodule JobBoard.JobController do
   use Phoenix.Controller
   alias JobBoard.Router
+  alias JobBoard.Jobs
+  alias JobBoard.Repo
+  alias JobBoard.Queries
 
   def index(conn, _params) do
-    jobs = JobBoard.Queries.jobs_query
+    jobs = Queries.jobs_query
     render conn, "index", jobs: jobs
   end
 
@@ -12,32 +15,32 @@ defmodule JobBoard.JobController do
   end
 
   def create(conn, params) do
-    job = %JobBoard.Jobs{title: params["title"], description: params["description"], job_type: params["type"], job_status: params["status"]}
-    JobBoard.Repo.insert(job)
+    job = %Jobs{title: params["title"], description: params["description"], job_type: params["type"], job_status: params["status"]}
+    Repo.insert(job)
     redirect conn, Router.pages_path(:index)
   end
 
   def show(conn, params) do
-    job = JobBoard.Queries.job_detail_query(params["id"])
+    job = Queries.job_detail_query(params["id"])
     render conn, "job", [job: job, action: params["action"]]
   end
 
   def edit(conn, %{"id" => id}) do
-    job = JobBoard.Queries.job_detail_query(id)
+    job = Queries.job_detail_query(id)
     render conn, "edit", jbo: job
   end
 
   def update(conn, params) do
     IO.inspect params["type"]
-    job = JobBoard.Repo.get(JobBoard.Jobs, params["id"])
+    job = Repo.get(Jobs, params["id"])
     job = %{job | title: params["title"], description: params["description"], job_type: params["type"], jbb_status: params["status"]}
-    JobBoard.Repo.update(job)
+    Repo.update(job)
     redirect conn, Router.pages_path(:index)
   end
 
   def destroy(conn, params) do
-    job = JobBoard.Queries.job_detail_query(params["id"])
-    JobBoard.Repo.delete(job)
+    job = Queries.job_detail_query(params["id"])
+    Repo.delete(job)
     redirect conn, Router.pages_path(:index)
   end
 end
