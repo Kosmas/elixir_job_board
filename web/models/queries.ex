8ee8254f9bb4cc73f8cdef2a4eced6db33056357
Kosmas Chatzimichalis <kosmas@mach7x.com>
2014-09-28 23:22:58 +0200

    clean out code by using aliases

--- web/models/queries.ex
+++ web/models/queries.ex
@@ -1,18 +1,20 @@
 defmodule JobBoard.Queries do
   import Ecto.Query
+  alias JobBoard.Jobs
+  alias JobBoard.Repo
 
   def jobs_query do
-    query = from job in JobBoard.Jobs,
+    query = from job in Jobs,
             order_by: [desc: job.id],
             select: job
-    JobBoard.Repo.all(query)
+    Repo.all(query)
   end
 
   def job_detail_query(id) do
     int_id = String.to_integer(id)
-    query = from job in JobBoard.Jobs,
+    query = from job in Jobs,
             where: job.id == ^int_id,
             select: job
-    JobBoard.Repo.all(query) |> List.first
+    Repo.all(query) |> List.first
   end
 end
